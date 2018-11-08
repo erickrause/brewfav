874152c26626b234eba9795730d3858cb380b5d6
Eric Krause <eric.krause@gmail.com>
2018-11-07 17:25:06 -0700

    annotate serializers

--- app/serializers/beer_serializer.rb
+++ app/serializers/beer_serializer.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: beers
+#
+#  id              :bigint(8)        not null, primary key
+#  brewery_db_json :jsonb
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  brewery_id      :bigint(8)
+#
+# Indexes
+#
+#  index_beers_on_brewery_id  (brewery_id)
+#
+
 class BeerSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :style, :availability, :update_date
   belongs_to :brewery
--- app/serializers/brewery_serializer.rb
+++ app/serializers/brewery_serializer.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: breweries
+#
+#  id              :bigint(8)        not null, primary key
+#  brewery_db_json :jsonb
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class BrewerySerializer < ActiveModel::Serializer
   attributes :id, :name
 end
