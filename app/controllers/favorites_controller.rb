1d1d447c75652b2768769dd1d2c0b60df5315266
Eric Krause <eric.krause@gmail.com>
2018-11-07 19:07:18 -0700

    can add beers to a user's favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,14 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user, only: [:index]
+  before_action :authenticate_user, only: [:index, :create]
 
   def index
     render json: current_user.beers
+  end
+
+  def create
+    beer = Beer.find(params[:beer_id])
+    current_user.beers << beer
+    render status: :created
   end
 
 end
--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Favorites", type: :request do
   let(:user) { User.create(email: 'foo@test.com', password: 'yagni123')}
+
+  let(:jwt) do
+        post user_token_path, params: {auth: {email: user.email, password: user.password}}
+        JSON.parse(response.body)["jwt"]
+      end
+
+  let(:jwt_header) {{'Authorization': "Bearer #{jwt}"}}
+
   describe "GET /favorites" do
     it "requires authentication" do
       get favorites_path
@@ -9,15 +17,31 @@ RSpec.describe "Favorites", type: :request do
     end
 
     describe "with authentication" do
-      let(:jwt) do
-        post user_token_path, params: {auth: {email: user.email, password: user.password}}
-        JSON.parse(response.body)["jwt"]
-      end
-
-      let(:jwt_header) {{'Authorization': "Bearer #{jwt}"}}
       it "returns 200" do
         get(favorites_path, headers: jwt_header)
         expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe "POST /favorites" do
+    let(:brewery) {Brewery.create}
+    let(:beer) { Beer.create(brewery_db_json: {}, brewery: brewery)}
+
+    it "requires authentication" do
+      post favorites_path
+      expect(response).to have_http_status(401)
+    end
+
+    describe "with auth" do
+      it "adds the beer to the user's favorites" do
+        post favorites_path, params: {beer_id: beer.id}, headers: jwt_header
+        expect(response).to have_http_status(:created)
+      end
+      it "adds the beer to the user's favorites" do
+        post favorites_path, params: {beer_id: beer.id}, headers: jwt_header
+        beer_array = JSON.parse response.body
+        expect(response).to have_http_status(:created)
       end
     end
   end
